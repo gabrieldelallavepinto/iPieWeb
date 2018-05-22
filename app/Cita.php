dd1d4a4dea5ae479c408486ee2e2d722f036bfdf
Manuel Díaz Gil <FrkWTF@gmail.com>
2018-05-22 19:16:57 +0200

    añadiendo validaciones

--- app/Cita.php
+++ app/Cita.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cita extends Model
 {
+
+    public $rules = [
+      'idTipo' => 'required',
+      'idClinica' => 'required',
+      'fecha' => 'required',
+      'idPodologo' => 'required'
+    ];
+
+
   protected $fillable = [
       'idCliente', 'idTipo', 'idClinica', 'fecha', 'idUsuario', 'idPodologo', 'notas'
      ];
--- app/Cliente.php
+++ app/Cliente.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
+
+
+  public $rules = [
+    'nombre' => 'required',
+    'apellidos' => 'required',
+    'tlfnFijo' => 'min:9|max:9',
+    'tlfnMovil' => 'min:9|max:9'
+  ];
+
   protected $fillable = [
       'nombre', 'apellidos', 'tlfnFijo','tlfnMovil'
   ];
